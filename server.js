c31dc688cd29191f97632065363cd413f6d70883
Brendan Beck <brendan.beck62@gmail.com>
2020-03-14 16:52:14 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const url = require('url');
+const favicon = require('express-favicon');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app.use(express.static("static"));
+app.use(favicon(__dirname + '/static/img/favicon.ico'));
+app.set('view engine', 'ejs');
+
+
+app.get(/^(?!\/api\/)/, (req, res) => {
+    let purl = url.parse(req.url, true);
+    let pathname = 'pages' + purl.pathname;
+
+    if ((pathname)[pathname.length - 1] === '/') {
+        pathname += 'index';
+    }
+    res.render(pathname, purl.query);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
