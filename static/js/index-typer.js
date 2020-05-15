967cbbc51709d491b04cbee609fcf4565b6a9acb
Brendan Beck <brendan.beck62@gmail.com>
2020-05-14 17:58:40 -0700

    better button placement

--- static/js/index-typer.js
+++ static/js/index-typer.js
@@ -4,7 +4,9 @@ var typewriter = new Typewriter(app, {
 });
 
 typewriter
-  .typeString('<span class="typer-h1">BRENDAN BECK</span><br>')
-  .pauseFor(1000)
-  .typeString('<span class="typer-h2">i make stuff look good</span>')
+  .typeString('<span class="typer-h1">BRENDEN')
+  .deleteChars(2)
+  .typeString('AN BECK</span><br>')
+  .pauseFor(500)
+  .typeString('<span class="typer-h2">i make stuff look good.</span>')
   .start();
---
+++ static/js/index.js
@@ -0,0 +1,7 @@
+
+window.onload=function() { //executes when the page finishes loading 
+    setTimeout(showBtn, 8000);
+};
+function showBtn() {
+    document.getElementById("resume-btn").className="show";
+}
