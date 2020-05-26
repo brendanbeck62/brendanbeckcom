1432f8935a9fce583d803c967a2dab1fbff72037
Brendan Beck <brendan.beck62@gmail.com>
2020-05-25 22:15:08 -0700

    update type text

--- static/js/index.js
+++ static/js/index.js
@@ -16,5 +16,5 @@ typewriter
   .deleteChars(2)
   .typeString('AN BECK</span><br>')
   .pauseFor(500)
-  .typeString('<span class="typer-h2">i make stuff look good.</span>')
+  .typeString('<span class="typer-h2">I make stuff look good.</span>')
   .start();
