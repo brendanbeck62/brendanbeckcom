987548aa164b13686318caf7af8417645bee429f
Brendan Beck <brendan.beck62@gmail.com>
2020-05-30 17:21:31 -0700

    made vscode theme download url

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.use(favicon(__dirname + '/static/img/favicon.ico'));
 app.set('view engine', 'ejs');
 
 
+// Download for the VSCode synthwave theme...
+// TODO remove this when full page is made for it
+app.get('/synthwave-theme', (req, res) => {
+    const file = `${__dirname}/static/downloads/Synthwave-Theme.vsix`;
+    res.download(file); // Set disposition and send it.
+});
+
 app.get(/^(?!\/api\/)/, (req, res) => {
     let purl = url.parse(req.url, true);
     let pathname = 'pages' + purl.pathname;
