ab15057b99beec81313bc6c3c7a8f6965c92db14
Brendan Beck <brendan.beck62@gmail.com>
2020-09-30 19:45:56 -0700

    commented out typer

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,13 @@
 
 
 // Typer
-var app = document.getElementById('type-text');
-var typewriter = new Typewriter(app, {
-});
-typewriter
-  .typeString('BRENDEN')
-  .deleteChars(2)
-  .typeString('AN BECK<br>')
-  .pauseFor(500)
-  .typeString('<span class="typer-h2">I write code the right way.</span>')
-  .start();
+// var app = document.getElementById('type-text');
+// var typewriter = new Typewriter(app, {
+// });
+// typewriter
+//   .typeString('BRENDEN')
+//   .deleteChars(2)
+//   .typeString('AN BECK<br>')
+//   .pauseFor(500)
+//   .typeString('<span class="typer-h2">I write code the right way.</span>')
+//   .start();
