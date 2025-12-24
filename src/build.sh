a0bdb7f1984eb4a6410984347c89d210493a0157
Brendan Beck <brendan.beck62@gmail.com>
2025-12-24 11:30:36 -0800

    comments

--- src/build.sh
+++ src/build.sh
@@ -3,8 +3,11 @@
 # number is the AWS account id
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 356960567614.dkr.ecr.us-west-2.amazonaws.com
 docker build -t brendanbeck62/bbcom . --platform=linux/amd64
-docker tag brendanbeck62/bbcom:latest 356960567614.dkr.ecr.us-west-2.amazonaws.com/brendanbeckcom-repo:latest
-docker push 356960567614.dkr.ecr.us-west-2.amazonaws.com/brendanbeckcom-repo:latest
+
+# TODO: need to set dev/prd for the repo name
+# TODO: need to version the containers
+docker tag brendanbeck62/bbcom:latest 356960567614.dkr.ecr.us-west-2.amazonaws.com/brendanbeckcom-repo-prod:latest
+docker push 356960567614.dkr.ecr.us-west-2.amazonaws.com/brendanbeckcom-repo-prod:latest
 
 #aws ecs update-service --cluster my-cluster --service my-first-service --force-new-deployment
 
